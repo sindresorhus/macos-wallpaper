ab3bdefe7e3f84def5b7b50ce86e4711c31ff18d
Sindre Sorhus <sindresorhus@gmail.com>
2015-04-03 13:10:04 +0700

    init

---
+++ readme.md
@@ -0,0 +1,41 @@
+# osx-wallpaper
+
+> Get or set the desktop wallpaper on OS X
+
+Should work on >=10.6, but only tested on 10.10.
+
+
+## Install
+
+### [npm](https://github.com/sindresorhus/node-osx-wallpaper#cli)
+
+```
+$ npm install --global osx-wallpaper
+```
+
+### Manually
+
+[Download the binary](https://github.com/sindresorhus/osx-wallpaper/releases/latest) and put it in `/usr/local/bin`.
+
+
+## Usage
+
+```sh
+# set
+wallpaper unicorn.jpg
+
+# get
+wallpaper
+> /Users/sindresorhus/unicorn.jpg
+```
+
+
+## Related
+
+- [`node-osx-wallpaper`](https://github.com/sindresorhus/node-osx-wallpaper) - Node wrapper.
+- [`wallpaper`](https://github.com/sindresorhus/wallpaper) - Get or set the desktop wallpaper.
+
+
+## License
+
+MIT © [Sindre Sorhus](http://sindresorhus.com)
---
+++ wallpaper/main.m
@@ -0,0 +1,34 @@
+//
+//  main.m
+//  wallpaper
+//
+//  Created by Sindre Sorhus on 27/03/15.
+//  Copyright (c) 2015 Sindre Sorhus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
+int main() {
+	@autoreleasepool {
+		NSArray *args = [[NSProcessInfo processInfo] arguments];
+		
+		if (args.count > 1) {
+			if ([args[1] isEqualToString: @"--version"]) {
+				puts("1.0.0");
+				return 0;
+			}
+			
+			if ([args[1] isEqualToString: @"--help"]) {
+				puts("\n  Get or set the desktop wallpaper\n\n  Usage: wallpaper [file]\n\n  Created by Sindre Sorhus");
+				return 0;
+			}
+			
+			[[NSWorkspace sharedWorkspace] setDesktopImageURL:[NSURL fileURLWithPath:args[1]] forScreen:[NSScreen mainScreen] options:nil error:nil];
+		} else {
+			printf("%s\n", [[[[NSWorkspace sharedWorkspace] desktopImageURLForScreen:[NSScreen mainScreen]] path] UTF8String]);
+		}
+	}
+	
+	return 0;
+}
