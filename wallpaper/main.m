74d73e32fa08e3bc6ee593b07554dde00a70dec7
Sindre Sorhus <sindresorhus@gmail.com>
2015-06-18 12:11:32 +0200

    minor tweaks

--- wallpaper/main.m
+++ wallpaper/main.m
@@ -6,29 +6,28 @@
 //  Copyright (c) 2015 Sindre Sorhus. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import <AppKit/AppKit.h>
+@import AppKit;
 
 int main() {
 	@autoreleasepool {
-		NSArray *args = [[NSProcessInfo processInfo] arguments];
-		
+		NSArray *args = [NSProcessInfo processInfo].arguments;
+
 		if (args.count > 1) {
 			if ([args[1] isEqualToString: @"--version"]) {
 				puts("1.0.0");
 				return 0;
 			}
-			
+
 			if ([args[1] isEqualToString: @"--help"]) {
 				puts("\n  Get or set the desktop wallpaper\n\n  Usage: wallpaper [file]\n\n  Created by Sindre Sorhus");
 				return 0;
 			}
-			
+
 			[[NSWorkspace sharedWorkspace] setDesktopImageURL:[NSURL fileURLWithPath:args[1]] forScreen:[NSScreen mainScreen] options:nil error:nil];
 		} else {
-			printf("%s\n", [[[[NSWorkspace sharedWorkspace] desktopImageURLForScreen:[NSScreen mainScreen]] path] UTF8String]);
+			printf("%s\n", [[NSWorkspace sharedWorkspace] desktopImageURLForScreen:[NSScreen mainScreen]].path.UTF8String);
 		}
 	}
-	
+
 	return 0;
 }
