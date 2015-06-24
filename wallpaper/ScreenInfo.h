ccd93e9da6dc7d010dfd6f05bb3d778436e383e6
Terence-Lee Davis <terence-lee.davis@edelman.com>
2015-06-24 17:58:42 +0100

    Cleaned up a few things

--- wallpaper/ScreenInfo.h
+++ wallpaper/ScreenInfo.h
@@ -3,14 +3,12 @@
 //  wallpaper
 //
 //  Created by Terence on 6/24/15.
-//  Copyright (c) 2015 Sindre Sorhus. All rights reserved.
 //
 
 @import AppKit;
 
 @interface ScreenInfo : NSObject
 + (NSArray*) getScreens;
-
 + (NSString*) getScreenDeviceName:(NSScreen*)screen;
 + (NSNumber*) getScreenDeviceID:(NSScreen*)screen;
 @end
--- wallpaper/ScreenInfo.m
+++ wallpaper/ScreenInfo.m
@@ -3,7 +3,6 @@
 //  wallpaper
 //
 //  Created by Terence on 6/24/15.
-//  Copyright (c) 2015 Sindre Sorhus. All rights reserved.
 //
 
 #import "ScreenInfo.h"
