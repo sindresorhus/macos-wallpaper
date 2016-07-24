397287d853dbc1953ade043e0110c3cb00d3f5b3
Ingwar Wirjawan <ingwar.wirjawan@gmail.com>
2016-07-25 00:16:12 +0700

    Fix case if the wallpaper is set to change every X minutes (#9)

--- wallpaper.m
+++ wallpaper.m
@@ -7,6 +7,7 @@
 //
 
 @import AppKit;
+#import <sqlite3.h>
 
 int main() {
 	@autoreleasepool {
@@ -38,7 +39,41 @@ int main() {
 				return 1;
 			}
 		} else {
-			printf("%s\n", [sw desktopImageURLForScreen:screen].path.UTF8String);
+			NSString *url = [sw desktopImageURLForScreen:screen].path;
+			BOOL isDir;
+			NSFileManager *fm = [NSFileManager defaultManager];
+
+			// check if file is a directory
+			[fm fileExistsAtPath:url isDirectory:&isDir];
+
+			// if directory, check db
+			if (isDir) {
+				NSArray *dirs = [fm URLsForDirectory:NSApplicationSupportDirectory
+																	 inDomains:NSUserDomainMask];
+				if ([dirs count] > 0) {
+					NSURL *dbLoc = [[dirs objectAtIndex:0] URLByAppendingPathComponent:@"Dock/desktoppicture.db"];
+					const char *dbPath = [[dbLoc path] UTF8String];
+					sqlite3 *db = nil;
+
+					if (sqlite3_open(dbPath, &db) == SQLITE_OK) {
+						sqlite3_stmt *statement;
+						const char *sql = "SELECT * FROM data";
+
+						if (sqlite3_prepare_v2(db, sql, -1, &statement, nil) == SQLITE_OK) {
+							NSString *file;
+							while (sqlite3_step(statement) == SQLITE_ROW) {
+								file = [NSString stringWithUTF8String:(char *)sqlite3_column_text(statement, 0)];
+							}
+
+							printf("%s/%s\n", url.UTF8String, file.UTF8String);
+							sqlite3_finalize(statement);
+						}
+						sqlite3_close(db);
+					}
+				}
+			} else {
+				printf("%s\n", url.UTF8String);
+			}
 		}
 	}
 
