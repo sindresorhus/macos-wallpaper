0fcc165d2146879df3840ad59ad1b86995d872b3
Sindre Sorhus <sindresorhus@gmail.com>
2018-09-26 11:27:19 +0700

    Upgrade to Swift 4.2

--- Package.swift
+++ Package.swift
@@ -1,17 +1,19 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
     name: "wallpaper",
     dependencies: [
-      .package(url: "https://github.com/jakeheis/SwiftCLI", from: "5.0.0"),
-      .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.11.4")
+        .package(url: "https://github.com/jakeheis/SwiftCLI", from: "5.2.0"),
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.11.5")
     ],
     targets: [
         .target(
             name: "wallpaper",
-            dependencies: ["SwiftCLI", "SQLite"]),
+            dependencies: [
+                "SwiftCLI",
+                "SQLite"
+            ]
+        ),
     ]
 )
